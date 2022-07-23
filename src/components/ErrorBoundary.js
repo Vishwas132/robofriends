c3778ddc3e97f2d206488115cbc059b271162157
Vishwas Lekhwar <64309462+Vishwas132@users.noreply.github.com>
2022-07-23 17:48:27 +0530

    added exception handler for CardList component

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,25 @@
+import React ,{Component} from "react";
+
+class ErrorBoundary extends Component {
+    constructor() {
+        super();
+        this.state = {
+            hasError : false
+        };
+    }
+
+    componentDidCatch() {
+        this.setState({
+            hasError : true,
+        });
+    }
+
+    render() {
+        if(this.state.hasError) {
+            return <h1>That is unexpected. Try refreshing the website.</h1>
+        }
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
